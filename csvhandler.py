17bcf64eda1dc9dcfbf5089d582807146af79d06
J3d4 <jeda@Asanari-WorkStation.localdomain>
2020-11-11 16:42:36 +0100

    Initial Commit

---
+++ csvhandler.py
@@ -0,0 +1,34 @@
+import csv
+from tqdm import tqdm
+
+
+class Dict:
+    def __init__(self, row, header):
+        self.__dict__ = dict(zip(header, row))
+
+
+class Read:
+    def dataframe(self, file):
+        return list(csv.reader(open(file, 'r', encoding='utf-8'), delimiter=','))
+
+
+class Write:
+    def header(self, data):
+        value = list()
+        for i in data[1].__dict__:
+            value.append(i)
+        return value
+
+    def writer(self, file, header, data):
+        write = csv.DictWriter(
+            open(file, 'w', encoding='utf-8'), delimiter=',', fieldnames=header)
+        write.writeheader()
+        for i in tqdm(range(len(data))):
+            write.writerow(data[i].__dict__)
+
+    def dataWriter(self, file, header, data):
+        write = csv.DictWriter(
+            open(file, 'w', encoding='utf-8'), delimiter=',', fieldnames=header)
+        write.writeheader()
+        for i in tqdm(data):
+            write.writerow(i)
---
+++ nace.py
@@ -0,0 +1,40 @@
+import csvhandler
+from tqdm import tqdm
+
+write = csvhandler.Write()
+read = csvhandler.Read()
+
+inp = read.dataframe('/mnt/d/Work/NACE/data/miskolc_out_in.csv')
+miskolc = [csvhandler.Dict(i, inp[0]) for i in inp[1:]]
+inp2 = read.dataframe('/mnt/d/Work/NACE/data/nace.csv')
+nace = [csvhandler.Dict(i, inp2[0]) for i in inp2[1:]]
+out = '/mnt/d/Work/NACE/data/miskolc_out_out.csv.csv'
+head = write.header(miskolc)
+##############################################################
+
+
+def calculate(nace, miskolc):
+    print("Calculations with nace in miskolc:")
+    for i in tqdm(range(len(miskolc))):
+        for j in range(len(nace)):
+            if miskolc[i].TEAOR08_fixed.strip() == nace[j].Kod.strip():
+                miskolc[i].SECTION = nace[j].SECTION
+                miskolc[i].DIVISION = nace[j].DIVISION
+                miskolc[i].GROUP = nace[j].GROUP
+                miskolc[i].CLASS = nace[j].CLASS
+
+    print("Miskolc done.")
+    return miskolc
+
+
+def writer(output, head, data):
+    print("Writing Miskolc-out")
+    write.writer(output, head, data)
+    print("Done.")
+
+
+# for debugging purposes:
+if __name__ == '__main__':
+    calculate(nace, miskolc)
+    writer(out, head, miskolc)
+    print(" ")
